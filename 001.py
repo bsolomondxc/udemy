acee7d88821285df471b8d74e1d51665ad54dbc9
bsolomondxc <44484916+bsolomondxc@users.noreply.github.com>
2019-02-08 11:11:14 +1100

    Create 001.py

---
+++ 001.py
@@ -0,0 +1,13 @@
+#This is my first Python file.
+
+int_x = 5
+int_y = 4.5
+
+print('The value of x = ',int_x)
+print('The value of y = ',int_y)
+print('                 ----')
+print('     So, x + y = ', (int_x + int_y))
+print(' ')
+print('The type of x is ',type(int_x))
+print('The type of y is ',type(int_y))
+print('The type of x+y is ',type(int_x+int_y))
